19688eba000721d6273b879804b70213dec7c028
kairy <951809209@qq.com>
2020-12-29 14:15:04 +0800

    libs2.0.0

--- mylibrary/src/main/java/com/example/mylibrary/MyUtils1.java
+++ mylibrary/src/main/java/com/example/mylibrary/MyUtils1.java
@@ -1,8 +1,19 @@
 package com.example.mylibrary;
 
-
 public class MyUtils1 {
-    public static String getS() {
+    public static String getS1() {
         return "hello word1";
+    }
+
+    public static String getS2() {
+        return "hello word2";
+    }
+
+    public static String getS3() {
+        return "hello word3";
+    }
+
+    public static String getS4() {
+        return "hello word4";
     }
 }
